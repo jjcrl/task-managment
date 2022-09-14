ddc29a6b30f14d26c3463c968b861c5d6f52d66f
jjcrl <j412cff@icloud.com>
2022-09-14 12:36:33 +0100

    on enter add item added

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Input = ({ setCurrItems }) => {
   const [input, setInput] = useState();
@@ -12,6 +12,13 @@ const Input = ({ setCurrItems }) => {
     setInput("");
   };
 
+  const onEnterPress = (e) => {
+    if (e.keyCode === 13) {
+      addItem();
+      handleSubmit();
+    }
+  };
+
   return (
     <>
       <div className="bg-green-100 py-3">
@@ -21,15 +28,11 @@ const Input = ({ setCurrItems }) => {
         <textarea
           onChange={(e) => setInput(e.target.value)}
           value={input}
-          className="w-full h-max text-4xl m-auto p-2 resize-none"
+          className="w-full h-40 text-4xl m-auto p-5 resize-none"
           placeholder="What do you have to do?"
           type="text"
+          onKeyDown={onEnterPress}
         />{" "}
-        {/* <button
-        className="w-full h-min text-6xl font-bold"
-        onClick={addItem}
-        type="submit"
-      ></button> */}
       </form>
     </>
   );
