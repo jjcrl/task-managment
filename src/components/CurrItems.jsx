72c4b413122583fb9dd56d703bd967310d1b0fe9
jjcrl <j412cff@icloud.com>
2022-09-14 18:42:51 +0100

    styling changes

--- src/components/CurrItems.jsx
+++ src/components/CurrItems.jsx
@@ -1,31 +1,23 @@
-import { Draggable } from "react-beautiful-dnd";
 import TaskCard from "./TaskCard";
-import { useState } from "react";
 
 const CurrItems = ({ currItems, setCurrItems, setPrevItems }) => {
-  const [show, setShow] = useState(true);
-
   const updateItem = (index) => {
     setPrevItems((prevItems) => [...prevItems, currItems[index]]);
     setCurrItems(currItems.filter((item) => item !== currItems[index]));
-    setShow(!show);
   };
 
   const removeItem = (index) => {
     setCurrItems(currItems.filter((item) => item !== currItems[index]));
-    setShow(!show);
   };
 
   return (
-    <ul className="w-full h-full p-8 bg-blue-100 columns-3">
+    <ul className="w-full h-full p-10 bg-neutral-900 columns-4">
       {currItems.map((item, index) => (
         <TaskCard
           item={item}
           index={index}
           removeItem={removeItem}
           updateItem={updateItem}
-          show={show}
-          setShow={setShow}
         />
       ))}
     </ul>
