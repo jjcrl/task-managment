5bd7034c7ff28c7a888e20d90af3bc0e6a18695c
jjcrl <j412cff@icloud.com>
2022-09-24 23:26:53 +0100

    mini added for week view

--- src/components/CurrItems.jsx
+++ src/components/CurrItems.jsx
@@ -6,6 +6,7 @@ const CurrItems = ({
   setPrevItems,
   classTest,
   day,
+  mini,
 }) => {
   const updateItem = (id) => {
     const [item] = currItems.filter((item) => item.id === id);
@@ -17,34 +18,52 @@ const CurrItems = ({
     setCurrItems(currItems.filter((item) => item.id !== id));
   };
 
-  return (
-    <div className={classTest}>
-      <ul className="w-fit h-full bg-neutral-800 mx-2">
-        {currItems.map((item, index) =>
-          !item.priority && item.dateChoice === day ? (
-            <TaskCard
-              item={item}
-              index={index}
-              removeItem={removeItem}
-              updateItem={updateItem}
-            />
-          ) : null
-        )}
-      </ul>
-      <ul className="w-fit h-full w-full h-full bg-neutral-800 mx-2">
-        {currItems.map((item, index) =>
-          item.priority && item.dateChoice === day ? (
-            <TaskCard
-              item={item}
-              index={index}
-              removeItem={removeItem}
-              updateItem={updateItem}
-            />
-          ) : null
-        )}
-      </ul>
-    </div>
-  );
+  const total = currItems.filter((item) => item.dateChoice === day).length;
+
+  const priototal = currItems.filter(
+    (item) => item.dateChoice === day && item.priority
+  ).length;
+
+  if (mini) {
+    return (
+      <div className="h-full">
+        <ul className="bg-none w-48 h-fit m-auto p-2 text-yellow-50 bg-neutral-800 p-3 text-left mt-10 text-xs leading-normal">
+          <li>•{total} items</li>
+          <li>•{priototal} prio</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className={classTest}>
+        <ul className="w-fit h-full bg-neutral-900 mx-2">
+          {currItems.map((item, index) =>
+            !item.priority && item.dateChoice === day ? (
+              <TaskCard
+                item={item}
+                index={index}
+                removeItem={removeItem}
+                updateItem={updateItem}
+              />
+            ) : null
+          )}
+        </ul>
+
+        <ul className="w-fit h-full w-full h-full bg-neutral-900 mx-2">
+          {currItems.map((item, index) =>
+            item.priority && item.dateChoice === day ? (
+              <TaskCard
+                item={item}
+                index={index}
+                removeItem={removeItem}
+                updateItem={updateItem}
+              />
+            ) : null
+          )}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default CurrItems;
