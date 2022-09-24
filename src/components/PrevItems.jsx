1afabb38e28ed92ae0fde88e1d93848261399912
jjcrl <j412cff@icloud.com>
2022-09-24 23:26:35 +0100

    ui ux changes

--- src/components/PrevItems.jsx
+++ src/components/PrevItems.jsx
@@ -1,23 +1,21 @@
 const PrevItems = ({ prevItems }) => {
+  const items = prevItems.slice(0, 6);
   return (
-    <div className="bg-neutral-900 h-full w-full flex flex-col overflow-hidden	truncate">
-      <ul className="w-ful p-2 text-right inline truncate">
+    <div className="h-fit w-full flex flex-col">
+      <ul className="w-full text-left pl-6">
         {prevItems.map((item, index) => (
-          <li
-            key={`item-${index}`}
-            className="text-3xl text-neutral-100 inline px-2 "
-          >
+          <li key={`item-${index}`} className="text-5xl inline p-1">
             {item.emoji}
           </li>
         ))}
       </ul>
-      <ul className="w-full h-14 py-2 truncate border-2 border-white overflow-hidden	">
-        {prevItems.map((item, index) => (
+      <ul className="w-fit h-fit bg-yellow-100 mr-auto p-3">
+        {items.map((item, index) => (
           <li
             key={`item-${index}`}
-            className="text-xl text-neutral-100 inline font-semibold px-1"
+            className="text-xs text-yellow-600 font-bold m-2 py-1 px-5  text-left w-fit  first-letter:text-lg"
           >
-            {item.input} {item.emoji}
+            {`${item.emoji} ${item.input.slice(0, 35)}..`}
           </li>
         ))}
       </ul>
