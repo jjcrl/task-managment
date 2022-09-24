b118c8a5db974cfa2d4d50a680b266de2ef2d287
jjcrl <j412cff@icloud.com>
2022-09-24 23:26:12 +0100

    date added

--- src/components/TodayView.jsx
+++ src/components/TodayView.jsx
@@ -1,12 +1,24 @@
 import CurrItems from "./CurrItems";
-const TodayView = ({ currItems, setCurrItems, setPrevItems }) => {
+const TodayView = ({ currItems, setCurrItems, setPrevItems, today }) => {
   return (
-    <div className="h-full w-full p-2">
+    <div className="h-full w-full p-2 bg-gray-900 pt-5">
+      <h1>
+        {today
+          .toLocaleDateString("en-US", {
+            weekday: "long",
+            day: "numeric",
+          })
+          .slice(0, 6)
+          .split(" ")
+          .reverse()
+          .join("")
+          .toUpperCase()}
+      </h1>
       <CurrItems
         currItems={currItems}
         setCurrItems={setCurrItems}
         setPrevItems={setPrevItems}
-        classTest="h-fit w-max flex flex-row gap-5 p-2"
+        classTest="h-fit w-fit flex flex-row gap-5 m-auto justify-between mt-5"
         day="day-1"
       />
     </div>
