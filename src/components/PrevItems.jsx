213760d987bd041bb28a69256fc004c30072fcca
jjcrl <j412cff@icloud.com>
2022-09-25 15:57:08 +0100

    minor styling changes

--- src/components/PrevItems.jsx
+++ src/components/PrevItems.jsx
@@ -1,21 +1,22 @@
 const PrevItems = ({ prevItems }) => {
   const items = prevItems.slice(0, 6);
+
   return (
     <div className="h-fit w-full flex flex-col">
       <ul className="w-full text-left pl-6">
-        {prevItems.map((item, index) => (
-          <li key={`item-${index}`} className="text-5xl inline p-1">
+        {items.map((item, index) => (
+          <li key={`item-${index}`} className="text-5xl inline p-2">
             {item.emoji}
           </li>
         ))}
       </ul>
-      <ul className="w-fit h-fit bg-yellow-100 mr-auto p-3">
+      <ul className="w-fit h-fit mr-auto p-3">
         {items.map((item, index) => (
           <li
             key={`item-${index}`}
-            className="text-xs text-yellow-600 font-bold m-2 py-1 px-5  text-left w-fit  first-letter:text-lg"
+            className="text-sm text-stone-500 font-bold m-2 px-5  text-left w-full first-letter:text-2xl"
           >
-            {`${item.emoji} ${item.input.slice(0, 35)}..`}
+            {`${item.emoji} ✅ ${item.input.slice(0, 35)}..`}
           </li>
         ))}
       </ul>
