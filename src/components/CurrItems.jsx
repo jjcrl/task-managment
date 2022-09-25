a6ef29df6d11dfa0d039f22497621d10eb2cb176
jjcrl <j412cff@icloud.com>
2022-09-25 15:56:32 +0100

    ux blobs added

--- src/components/CurrItems.jsx
+++ src/components/CurrItems.jsx
@@ -26,17 +26,33 @@ const CurrItems = ({
 
   if (mini) {
     return (
-      <div className="h-full">
-        <ul className="bg-none w-48 h-fit m-auto p-2 text-yellow-50 bg-neutral-800 p-3 text-left mt-10 text-xs leading-normal">
-          <li>•{total} items</li>
-          <li>•{priototal} prio</li>
+      <div className={classTest}>
+        <ul className="w-5/6 m-auto h-min rounded-2xl h-full text-stone-800 bg-yellow-200 text-xs flex flex-col mt-4 pb-4">
+          <div className="w-full h-fit relative flex pl-3 pt-4">
+            <span
+              className="h-10 w-10 bg-yellow-300  border-2 border-yellow-200"
+              id="rounded"
+            ></span>
+            <li className="tracking-widest bg-yellow-300 h-fit w-fit rounded-3xl p-4 ml-3">
+              {total} !tems
+            </li>
+          </div>
+          <div className="w-full h-fit relative flex pl-3 pt-4">
+            <span
+              className="h-10 w-10 bg-yellow-300  border-2 border-yellow-200"
+              id="rounded"
+            ></span>
+            <li className="tracking-widest bg-yellow-300 h-fit w-fit rounded-3xl p-4 ml-3">
+              {priototal} pr!o
+            </li>
+          </div>
         </ul>
       </div>
     );
   } else {
     return (
       <div className={classTest}>
-        <ul className="w-fit h-full bg-neutral-900 mx-2">
+        <ul className="w-full h-full bg-yellow-200 rounded-3xl flex flex-col m-auto px-4 gap-1 pb-2 pt-5">
           {currItems.map((item, index) =>
             !item.priority && item.dateChoice === day ? (
               <TaskCard
@@ -49,7 +65,7 @@ const CurrItems = ({
           )}
         </ul>
 
-        <ul className="w-fit h-full w-full h-full bg-neutral-900 mx-2">
+        <ul className="flex flex-col bg-yellow-200 h-full pt-5 px-4 m-auto rounded-2xl gap-1 pb-2 w-full">
           {currItems.map((item, index) =>
             item.priority && item.dateChoice === day ? (
               <TaskCard
