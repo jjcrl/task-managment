670ae4d4b34494a37dd6897a778f6f6ab6320348
jjcrl <j412cff@icloud.com>
2022-09-28 15:02:20 +0100

    formate date func now on all, func imporved

--- src/components/ThreeDayView.jsx
+++ src/components/ThreeDayView.jsx
@@ -8,58 +8,42 @@ const ThreeDayView = ({
   dateChoice,
   today,
 }) => {
-  const dayone = today
-    .toLocaleDateString("en-US", {
-      weekday: "long",
-      day: "numeric",
-    })
-    .slice(0, 6)
-    .split(" ")
-    .reverse()
-    .join("")
-    .toUpperCase();
+  const formatDate = (date) => {
+    const string = date
+      .toLocaleDateString("en-US", {
+        weekday: "long",
+        day: "numeric",
+      })
+      .toUpperCase()
+      .split(" ")
+      .reverse();
+    const day = string[0].slice(0, 3);
+    const number = string[1];
+    return +string[1] < 10 ? `${day}0${number}` : `${day}${number}`;
+  };
 
-  const daytwo = new Date(today.getTime() + 60 * 60 * 24 * 1000)
-    .toLocaleDateString("en-US", {
-      weekday: "long",
-      day: "numeric",
-    })
-    .slice(0, 6)
-    .split(" ")
-    .reverse()
-    .join("")
-    .toUpperCase();
-
-  const daythree = new Date(today.getTime() + 60 * 60 * 48 * 1000)
-    .toLocaleDateString("en-US", {
-      weekday: "long",
-      day: "numeric",
-    })
-    .slice(0, 6)
-    .split(" ")
-    .reverse()
-    .join("")
-    .toUpperCase();
+  const daytwo = new Date(today.getTime() + 60 * 60 * 24 * 1000);
+  const daythree = new Date(today.getTime() + 60 * 60 * 48 * 1000);
 
   return (
     <div className="h-full w-full grid grid-rows-1 grid-cols-3 bg-stone-50">
       <div
         className={
           dateChoice === "day-1"
-            ? "flex flex-col gap-5 bg-stone-100"
-            : "flex flex-col gap-5"
+            ? "flex flex-col gap-5 bg-stone-300"
+            : "flex flex-col gap-5 bg-stone-100"
         }
         onClick={() => {
           setDateChoice("day-1");
         }}
       >
-        <h2 className="text-stone-700">{dayone}</h2>
+        <h2 className="text-stone-700">{formatDate(today)}</h2>
         {
           <CurrItems
             currItems={currItems.filter((item) => item.dateChoice === "day-1")}
             setCurrItems={setCurrItems}
             setPrevItems={setPrevItems}
-            classTest="h-full w-full flex flex-col gap-8 px-3"
+            classTest="h-fit w-fit flex flex-col gap-6 px-5"
             day="day-1"
           />
         }
@@ -67,21 +51,21 @@ const ThreeDayView = ({
       <div
         className={
           dateChoice === "day-2"
-            ? "flex flex-col gap-5 bg-stone-100"
-            : "flex flex-col gap-5 "
+            ? "flex flex-col gap-5 bg-stone-300"
+            : "flex flex-col gap-5 bg-stone-100"
         }
         onClick={() => {
           setDateChoice("day-2");
         }}
       >
         {" "}
-        <h2 className="text-stone-700">{daytwo}</h2>
+        <h2 className="text-stone-700">{formatDate(daytwo)}</h2>
         {
           <CurrItems
             currItems={currItems.filter((item) => item.dateChoice === "day-2")}
             setCurrItems={setCurrItems}
             setPrevItems={setPrevItems}
-            classTest="h-fit w-fit flex flex-col gap-8 px-3"
+            classTest="h-fit w-fit flex flex-col gap-6 px-5"
             day="day-2"
           />
         }
@@ -89,21 +73,21 @@ const ThreeDayView = ({
       <div
         className={
           dateChoice === "day-3"
-            ? "flex flex-col gap-5 bg-stone-100"
-            : "flex flex-col gap-5"
+            ? "flex flex-col gap-5 bg-stone-300"
+            : "flex flex-col gap-5 bg-stone-100"
         }
         onClick={() => {
           setDateChoice("day-3");
         }}
       >
         {" "}
-        <h2 className="text-stone-700">{daythree}</h2>{" "}
+        <h2 className="text-stone-700">{formatDate(daythree)}</h2>{" "}
         {
           <CurrItems
             currItems={currItems.filter((item) => item.dateChoice === "day-3")}
             setCurrItems={setCurrItems}
             setPrevItems={setPrevItems}
-            classTest="h-fit w-fit flex flex-col gap-8 px-3"
+            classTest="h-fit w-fit flex flex-col gap-6 px-5"
             day="day-3"
           />
         }
--- src/components/WeekView.jsx
+++ src/components/WeekView.jsx
@@ -8,28 +8,18 @@ const WeekView = ({
   dateChoice,
   today,
 }) => {
-  const dayone = today
-    .toLocaleDateString("en-US", {
-      weekday: "long",
-      day: "numeric",
-    })
-    .slice(0, 6)
-    .split(" ")
-    .reverse()
-    .join("")
-    .toUpperCase();
-
-  const formateDate = (date) => {
-    return date
+  const formatDate = (date) => {
+    const string = date
       .toLocaleDateString("en-US", {
         weekday: "long",
         day: "numeric",
       })
-      .slice(0, 6)
+      .toUpperCase()
       .split(" ")
-      .reverse()
-      .join("")
-      .toUpperCase();
+      .reverse();
+    const day = string[0].slice(0, 3);
+    const number = string[1];
+    return +string[1] < 10 ? `${day}0${number}` : `${day}${number}`;
   };
 
   const daytwo = new Date(today.getTime() + 60 * 60 * 24 * 1000);
@@ -44,20 +34,20 @@ const WeekView = ({
       <div
         className={
           dateChoice === "day-1"
-            ? "h-full w-full bg-stone-100 flex flex-col"
-            : "h-full w-full bg-stone-50 flex flex-col"
+            ? "h-full w-full bg-stone-300 flex flex-col"
+            : "h-full w-full bg-stone-100 flex flex-col"
         }
         onClick={() => {
           setDateChoice("day-1");
         }}
       >
-        <h2 className="text-stone-700">{dayone}</h2>
+        <h2 className="text-stone-700">{formatDate(today)}</h2>
         {
           <CurrItems
             currItems={currItems.filter((item) => item.dateChoice === "day-1")}
             setCurrItems={setCurrItems}
             setPrevItems={setPrevItems}
-            classTest="h-full w-full flex flex-col"
+            classTest="h-fit w-full flex flex-col"
             day="day-1"
             mini={true}
           />
@@ -66,20 +56,20 @@ const WeekView = ({
       <div
         className={
           dateChoice === "day-2"
-            ? "h-full w-full bg-stone-100 flex flex-col"
-            : "h-full w-full bg-stone-50 flex flex-col"
+            ? "h-full w-full bg-stone-300 flex flex-col"
+            : "h-full w-full bg-stone-100 flex flex-col"
         }
         onClick={() => {
           setDateChoice("day-2");
         }}
       >
-        <h2 className="text-stone-700">{formateDate(daytwo)}</h2>
+        <h2 className="text-stone-700">{formatDate(daytwo)}</h2>
         {
           <CurrItems
             currItems={currItems.filter((item) => item.dateChoice === "day-2")}
             setCurrItems={setCurrItems}
             setPrevItems={setPrevItems}
-            classTest="h-full w-full flex flex-col"
+            classTest="h-fit w-full flex flex-col"
             day="day-2"
             mini={true}
           />
@@ -88,21 +78,21 @@ const WeekView = ({
       <div
         className={
           dateChoice === "day-3"
-            ? "h-full w-full bg-stone-100 flex flex-col"
-            : "h-full w-full bg-stone-50 flex flex-col"
+            ? "h-full w-full bg-stone-300 flex flex-col"
+            : "h-full w-full bg-stone-100 flex flex-col"
         }
         onClick={() => {
           setDateChoice("day-3");
         }}
       >
-        <h2 className="text-stone-700">{formateDate(daythree)}</h2>
+        <h2 className="text-stone-700">{formatDate(daythree)}</h2>
         {
           <CurrItems
             currItems={currItems.filter((item) => item.dateChoice === "day-3")}
             setCurrItems={setCurrItems}
             setPrevItems={setPrevItems}
             mini={true}
-            classTest="h-full w-full flex flex-col "
+            classTest="h-fit w-full flex flex-col "
             day="day-3"
           />
         }
@@ -110,20 +100,20 @@ const WeekView = ({
       <div
         className={
           dateChoice === "day-4"
-            ? "h-full w-full bg-stone-100 flex flex-col"
-            : "h-full w-full bg-stone-50 flex flex-col"
+            ? "h-full w-full bg-stone-300 flex flex-col"
+            : "h-full w-full bg-stone-100 flex flex-col"
         }
         onClick={() => {
           setDateChoice("day-4");
         }}
       >
-        <h2 className="text-stone-700">{formateDate(dayfour)}</h2>
+        <h2 className="text-stone-700">{formatDate(dayfour)}</h2>
         {
           <CurrItems
             currItems={currItems.filter((item) => item.dateChoice === "day-4")}
             setCurrItems={setCurrItems}
             setPrevItems={setPrevItems}
-            classTest="h-full w-full flex flex-col"
+            classTest="h-fit w-full flex flex-col"
             day="day-4"
             mini={true}
           />
@@ -132,20 +122,20 @@ const WeekView = ({
       <div
         className={
           dateChoice === "day-5"
-            ? "h-full w-full bg-stone-100 flex flex-col"
-            : "h-full w-full bg-stone-50 flex flex-col"
+            ? "h-full w-full bg-stone-300 flex flex-col"
+            : "h-full w-full bg-stone-100 flex flex-col"
         }
         onClick={() => {
           setDateChoice("day-5");
         }}
       >
-        <h2 className="text-stone-700">{formateDate(dayfive)}</h2>
+        <h2 className="text-stone-700">{formatDate(dayfive)}</h2>
         {
           <CurrItems
             currItems={currItems.filter((item) => item.dateChoice === "day-5")}
             setCurrItems={setCurrItems}
             setPrevItems={setPrevItems}
-            classTest="h-full w-full flex flex-col"
+            classTest="h-fit w-full flex flex-col"
             day="day-5"
             mini={true}
           />
@@ -154,21 +144,21 @@ const WeekView = ({
       <div
         className={
           dateChoice === "day-6"
-            ? "h-full w-full bg-stone-100 flex flex-col"
-            : "h-full w-full bg-stone-50 flex flex-col"
+            ? "h-full w-full bg-stone-300 flex flex-col"
+            : "h-full w-full bg-stone-100 flex flex-col"
         }
         onClick={() => {
           setDateChoice("day-6");
         }}
       >
-        <h2 className="text-stone-700">{formateDate(daysix)}</h2>
+        <h2 className="text-stone-700">{formatDate(daysix)}</h2>
 
         {
           <CurrItems
             currItems={currItems.filter((item) => item.dateChoice === "day-6")}
             setCurrItems={setCurrItems}
             setPrevItems={setPrevItems}
-            classTest="h-full w-full flex flex-col"
+            classTest="h-fit w-full flex flex-col"
             day="day-6"
             mini={true}
           />
@@ -177,20 +167,20 @@ const WeekView = ({
       <div
         className={
           dateChoice === "day-7"
-            ? "h-full w-full bg-stone-100 flex flex-col"
-            : "h-full w-full bg-stone-50 flex flex-col"
+            ? "h-full w-full bg-stone-300 flex flex-col"
+            : "h-full w-full bg-stone-100 flex flex-col"
         }
         onClick={() => {
           setDateChoice("day-7");
         }}
       >
-        <h2 className="text-stone-700">{formateDate(dayseven)}</h2>
+        <h2 className="text-stone-700">{formatDate(dayseven)}</h2>
         {
           <CurrItems
             currItems={currItems.filter((item) => item.dateChoice === "day-7")}
             setCurrItems={setCurrItems}
             setPrevItems={setPrevItems}
-            classTest="h-full w-full flex flex-col"
+            classTest="h-fit w-full flex flex-col"
             day="day-7"
             mini={true}
           />
