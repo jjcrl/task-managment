544c3b60f065de04d1f3167031c4660a802e1a5c
jjcrl <j412cff@icloud.com>
2022-09-28 15:49:10 +0100

    minor styling changes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,11 @@
 const Navigation = ({ setView }) => {
   return (
-    <nav className="w-min h-min flex flex-row bg-stone-200 justify-end gap-5 px-3 py-3 text-yellow-500 absolute right-10 bottom-10">
+    <nav className="bg-stone-200 px-3 py-3  absolute right-10 bottom-10 rounded-3xl">
       <button
         onClick={() => {
           setView("today");
         }}
+        className="rounded-l-2xl"
       >
         1
       </button>
@@ -19,6 +20,7 @@ const Navigation = ({ setView }) => {
         onClick={() => {
           setView("week");
         }}
+        className="rounded-r-2xl"
       >
         7
       </button>
