a3dcd05306e1d1e90718d1c2aec728a38603e2f0
jjcrl <j412cff@icloud.com>
2022-09-28 15:39:00 +0100

    day one totals added

--- src/components/TodayView.jsx
+++ src/components/TodayView.jsx
@@ -18,7 +18,7 @@ const TodayView = ({
         setDateChoice("day-1");
       }}
     >
-      <h2 className="text-stone-700 w-full text-left ml-5">
+      <h1 className="text-stone-700 w-full text-left ml-5 p-2">
         {today
           .toLocaleDateString("en-US", {
             weekday: "long",
@@ -29,12 +29,29 @@ const TodayView = ({
           .reverse()
           .join("")
           .toUpperCase()}
-      </h2>
+      </h1>
+
+      <div className="rounded-2xl w-fit bg-stone-400 text-stone-50 flex flex-col ml-10 p-5 text-left px-7 shadow-md absolute top-10 right-10 ">
+        {" "}
+        <h3>
+          Total items{" "}
+          {currItems.filter((item) => item.dateChoice === "day-1").length}
+        </h3>
+        <h3>
+          Priority items{" "}
+          {
+            currItems.filter(
+              (item) => item.dateChoice === "day-1" && item.priority
+            ).length
+          }
+        </h3>
+      </div>
+
       <CurrItems
         currItems={currItems}
         setCurrItems={setCurrItems}
         setPrevItems={setPrevItems}
-        classTest="h-fit w-fit flex flex-col gap-5 m-auto justify-between mt-10"
+        classTest="h-fit w-full flex flex-row gap-8 m-auto justify-between mt-16 px-10"
         day="day-1"
       />
     </div>
