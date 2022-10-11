4ace144e63274a8f4a26a3aefaad9fabc981d56e
jjcrl <j412cff@icloud.com>
2022-10-11 18:26:56 +0100

    remove test data , add days to app pass via props

--- src/App.js
+++ src/App.js
@@ -6,221 +6,10 @@ import TodayView from "./components/TodayView";
 import ThreeDayView from "./components/ThreeDayView";
 import WeekView from "./components/WeekView";
 import Navigation from "./components/Navigation";
+import { formatDate } from "./components/formatDate";
 
 function App() {
-  const [currItems, setCurrItems] = useState([
-    {
-      input: "Work on the previs items section.",
-      priority: true,
-      emoji: "💯",
-      dateChoice: "day-1",
-      id: 0,
-    },
-    {
-      input: "Fix the emoji list",
-      priority: true,
-      emoji: "🐝",
-      dateChoice: "day-1",
-      id: 1,
-    },
-    {
-      input: "Dispaly today in a better way with 2 lists",
-      priority: true,
-      emoji: "🏆",
-      dateChoice: "day-1",
-      id: 2,
-    },
-    {
-      input: "Add lists and views for 3 day ",
-      priority: false,
-      emoji: "🎉",
-      dateChoice: "day-1",
-      id: 3,
-    },
-    {
-      input: "Drag and drop abilites",
-      priority: false,
-      emoji: "💯",
-      dateChoice: "day-1",
-      id: 4,
-    },
-    {
-      input: "Local storage for user",
-      priority: false,
-      emoji: "⚡",
-      dateChoice: "day-1",
-      id: 5,
-    },
-    {
-      input: "Email over to do lists",
-      priority: false,
-      emoji: "🌟",
-      dateChoice: "day-1",
-      id: 6,
-    },
-    {
-      input: "Priortity times and dates ",
-      priority: false,
-      emoji: "🙌",
-      dateChoice: "day-1",
-      id: 7,
-    },
-    {
-      input: "microinterationcs and animations",
-      priority: false,
-      emoji: "🎇",
-      dateChoice: "day-1",
-      id: 8,
-    },
-    {
-      input: "go to the shops for a vape",
-      priority: false,
-      emoji: "⚡",
-      dateChoice: "day-2",
-      id: 9,
-    },
-    {
-      input: "quit vaping",
-      priority: true,
-      emoji: "✅",
-      dateChoice: "day-2",
-      id: 10,
-    },
-    {
-      input: "visit grandma",
-      priority: true,
-      emoji: "❗️",
-      dateChoice: "day-2",
-      id: 11,
-    },
-    {
-      input: "go for a walk ",
-      priority: false,
-      emoji: "🏆",
-      dateChoice: "day-2",
-      id: 12,
-    },
-    {
-      input: "stretch out",
-      priority: false,
-      emoji: "🌱",
-      dateChoice: "day-2",
-      id: 13,
-    },
-    {
-      input: "post on linked in ",
-      priority: false,
-      emoji: "🌱",
-      dateChoice: "day-2",
-      id: 14,
-    },
-    {
-      input: "apply for jobs",
-      priority: false,
-      emoji: "✨",
-      dateChoice: "day-3",
-      id: 15,
-    },
-    {
-      input: "post on linked in ",
-      priority: false,
-      emoji: "🎆",
-      dateChoice: "day-3",
-      id: 16,
-    },
-    {
-      input: "do some code art",
-      priority: false,
-      emoji: "❗️",
-      dateChoice: "day-3",
-      id: 17,
-    },
-    {
-      input: "change cv and apply for mroe jobs ",
-      priority: false,
-      emoji: "🏁",
-      dateChoice: "day-3",
-      id: 18,
-    },
-    {
-      input: "go for a walk ",
-      priority: true,
-      emoji: "🚂",
-      dateChoice: "day-3",
-      id: 19,
-    },
-    {
-      input: "stretch",
-      priority: true,
-      emoji: "⭐️",
-      dateChoice: "day-3",
-      id: 20,
-    },
-    {
-      input: "visit grandma",
-      priority: true,
-      emoji: "🐝",
-      dateChoice: "day-3",
-      id: 21,
-    },
-    {
-      input: "this is a test for a fourth day item ",
-      priority: false,
-      emoji: "🙌",
-      dateChoice: "day-4",
-      id: 22,
-    },
-    {
-      input: "this is a test for a fourth day item",
-      priority: true,
-      emoji: "🌟",
-      dateChoice: "day-4",
-      id: 23,
-    },
-    {
-      input: "prio testing on week view",
-      priority: true,
-      emoji: "❗️",
-      dateChoice: "day-5",
-      id: 24,
-    },
-    {
-      input: "\ntesting for week view ",
-      priority: false,
-      emoji: "💯",
-      dateChoice: "day-6",
-      id: 25,
-    },
-    {
-      input: "testing for week view",
-      priority: false,
-      emoji: "🔧",
-      dateChoice: "day-6",
-      id: 26,
-    },
-    {
-      input: "week view testing",
-      priority: false,
-      emoji: "🥳",
-      dateChoice: "day-7",
-      id: 27,
-    },
-    {
-      input: "week view testing",
-      priority: true,
-      emoji: "💡",
-      dateChoice: "day-7",
-      id: 28,
-    },
-    {
-      input: "week view testing",
-      priority: true,
-      emoji: "🌟",
-      dateChoice: "day-7",
-      id: 29,
-    },
-  ]);
-  //_↗
+  const [currItems, setCurrItems] = useState([]);
   const [prevItems, setPrevItems] = useState([]);
   const [view, setView] = useState("today");
   const [dateChoice, setDateChoice] = useState();
@@ -233,6 +22,23 @@ function App() {
   );
 
   const today = new Date();
+  const day1 = formatDate(today);
+  const day2 = formatDate(new Date(today.getTime() + 60 * 60 * 24 * 1000));
+  const day3 = formatDate(new Date(today.getTime() + 60 * 60 * 48 * 1000));
+  const day4 = formatDate(new Date(today.getTime() + 60 * 60 * 72 * 1000));
+  const day5 = formatDate(new Date(today.getTime() + 60 * 60 * 96 * 1000));
+  const day6 = formatDate(new Date(today.getTime() + 60 * 60 * 120 * 1000));
+  const day7 = formatDate(new Date(today.getTime() + 60 * 60 * 148 * 1000));
+
+  const taskDateOptions = {
+    "day-one": day1,
+    "day-2": day2,
+    "day-three": day3,
+    "day-four": day4,
+    "day-five": day5,
+    "day-six": day6,
+    "day-seven": day7,
+  };
 
   return (
     <div className="App">
@@ -244,6 +50,8 @@ function App() {
             setCurrItems={setCurrItems}
             dateChoice={dateChoice}
             view={view}
+            today={today}
+            taskDateOptions={taskDateOptions}
           />
           <div className="w-min h-min text-6xl text-slate-700 ml-auto p-5">
             <h2>{percent ? `${percent}%` : "0%"}</h2>
@@ -257,9 +65,11 @@ function App() {
               currItems={currItems}
               setCurrItems={setCurrItems}
               setPrevItems={setPrevItems}
-              today={today}
+              days={[day1]}
               dateChoice={dateChoice}
               setDateChoice={setDateChoice}
+              taskDateOptions={taskDateOptions}
+              today={day1}
             />
           ) : view === "three-day" ? (
             <ThreeDayView
@@ -268,7 +78,7 @@ function App() {
               setPrevItems={setPrevItems}
               setDateChoice={setDateChoice}
               dateChoice={dateChoice}
-              today={today}
+              days={[day1, day2, day3]}
             />
           ) : (
             <WeekView
@@ -277,7 +87,7 @@ function App() {
               setPrevItems={setPrevItems}
               setDateChoice={setDateChoice}
               dateChoice={dateChoice}
-              today={today}
+              days={[day1, day2, day3, day4, day5, day6, day7]}
             />
           )}
         </section>
