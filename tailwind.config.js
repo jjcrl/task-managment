5822f327cddd069b416051d459404efd303156ea
jjcrl <j412cff@icloud.com>
2022-10-14 11:13:31 +0100

    extra styles added to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    minHeight: {
+      "3/4": "75%",
+      "1/2": "50%",
+    },
+    maxHeight: {
+      "5/6": "83.333333%",
+    },
     extend: {},
   },
   plugins: [],
