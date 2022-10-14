fd410403e22981f02c672e81f362ad0c449a5c01
jjcrl <j412cff@icloud.com>
2022-10-14 11:14:29 +0100

    no scroll fixed

--- src/components/TodayView.jsx
+++ src/components/TodayView.jsx
@@ -10,7 +10,7 @@ const TodayView = ({
 }) => {
   return (
     <div
-      className="h-full w-full bg-white"
+      className="h-screen w-full bg-white"
       onClick={() => {
         setDateChoice("day-1");
       }}
@@ -28,7 +28,7 @@ const TodayView = ({
         currItems={currItems}
         setCurrItems={setCurrItems}
         setPrevItems={setPrevItems}
-        classTest="h-full w-full flex flex-row gap-8 px-10"
+        classTest="h-full w-full flex flex-row gap-8 px-10 overflow-scroll"
         day="day-1"
         taskDateOptions={taskDateOptions}
       />
